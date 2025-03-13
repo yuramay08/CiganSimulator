fb3285afd47a8a5fd14510b88c4eda1b6ce36c1b
Yurii Kuza <yuramay08@gmail.com>
2025-03-13 16:17:44 +0100

    cigan simulator black edition

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Graphics.OpenGL4;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var settings = new GameWindowSettings();
+        var nativeSettings = new NativeWindowSettings()
+        {
+            Size = new OpenTK.Mathematics.Vector2i(800, 600),   //resoultion of window
+            Title = "Cigan Simulator"                           //title
+        };
+
+        using (var window = new GameWindow(settings, nativeSettings))
+        {
+            window.Run();
+        }
+    }
+}
