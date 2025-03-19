d734e39523d958cd38044a66d502864f3b5d4d8e
Yurii Kuza <yuramay08@gmail.com>
2025-03-19 08:45:10 +0100

    Implement game update and render logic; add escape key functionality to close the game(NO)

--- Game.cs
+++ Game.cs
@@ -10,10 +10,31 @@ namespace CiganSimulator
         public Game(int width, int height, string title) 
         : base(GameWindowSettings.Default, new NativeWindowSettings() 
         { Size = (width, height), Title = title }) 
-        { 
+        {
 
         }
 
-        // Add your game logic here
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code
+
+            SwapBuffers();
+        }
     }
 }
